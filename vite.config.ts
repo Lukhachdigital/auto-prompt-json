a60cb01c4b60598f20584ab7a593227e1a40f3fa
Lukhachdigital <huynhxuyensondirector@gmail.com>
2025-10-16 12:41:26 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -16,7 +17,8 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          // FIX: `__dirname` is not available in ES modules. Use `import.meta.url` to get the current directory path.
+          '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.'),
         }
       }
     };
